e635cd5b2a03211a38e9eae792d2dcb4fd01d250
aamerino <amerinotsutsui@gmail.com>
2017-05-24 20:14:50 +0200

    Lol

--- KLog/views.py
+++ KLog/views.py
@@ -8,7 +8,7 @@ from KLog.LogBuilders.Log4JavaParser import Log4JavaParser
 class FileUpload(viewsets.ModelViewSet):
     parser_classes = (Log4JavaParser,)
 
-    def put(self, request, file, format=None):
+    def put(self, request, file=None, format=None):
         file = self.request.FILES['file']
         return Response(status=status.HTTP_400_BAD_REQUEST)
         return file.readline()
