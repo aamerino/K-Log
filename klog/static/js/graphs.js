89291c73cd40472fb9d717a94a8fecb0ea28beee
aamerino <amerinotsutsui@gmail.com>
2017-06-06 00:39:10 +0200

    Deploy

--- klog/static/js/graphs.js
+++ klog/static/js/graphs.js
@@ -1,7 +1,11 @@
 
 function pieChart(pD){
-    console.log(d3.json('https://pure-beach-44803.herokuapp.com/client/firstgraph'));
-    function segColor(c){ return {low:"#807dba", mid:"#e08214",high:"#41ab5d"}[c]; }
+    console.log(d3.json('https://pure-beach-44803.herokuapp.com/client/firstgraph', function(error,data){
+        console.log(data);
+    }))
+
+
+        function segColor(c){ return {low:"#807dba", mid:"#e08214",high:"#41ab5d"}[c]; }
     var pC ={},    pieDim ={w:250, h: 250};
     pieDim.r = Math.min(pieDim.w, pieDim.h) / 2;
     var id = '#dashboard';
