df9893b64dd84cdf8fafc5d71217895dd8c59723
Dual <amerinotsutsui@gmail.com>
2017-06-09 16:48:05 +0200

    Deploy

--- klog/static/js/graphs.js
+++ klog/static/js/graphs.js
@@ -1,85 +1,86 @@
-function getDataGraph(callback) {
-    d3.json('http://localhost:8000/api/v1/client/countExceptions', function (error, data) {
-        callback(data);
-    })
+function getDomain() {
+    return location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '');
 }
-
-function pieChart(datarino) {
+function pieChart() {
     function segColor(c) {
         return {low: "#807dba", mid: "#e08214", high: "#41ab5d"}[c];
     }
 
-    var color = d3.scaleOrdinal(d3.schemeCategory20);
+    d3.json(getDomain() + '/api/v1/client/countExceptions', function (error, data) {
 
-    var pC = {}, pieDim = {w: 600, h: 600};
-    pieDim.r = Math.min(pieDim.w, pieDim.h) / 2;
-    var id = '#dashboard';
+        var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-    // create svg for pie chart.
-    var piesvg = d3.select(id).append("svg")
-        .attr("width", 800)
-        .attr("height", 800)
-        .append("g")
-        .attr("transform", "translate(" + (pieDim.w + 300) / 2 + "," + (pieDim.h + 300) / 2 + ")");
+        var pC = {}, pieDim = {w: 600, h: 600};
+        pieDim.r = Math.min(pieDim.w, pieDim.h) / 2;
+        var id = '#dashboard';
 
-    var w = 0;
-    var outerRadius = w / 2;
+        // create svg for pie chart.
+        var piesvg = d3.select(id).append("svg")
+            .attr("width", 800)
+            .attr("height", 800)
+            .append("g")
+            .attr("transform", "translate(" + (pieDim.w + 300) / 2 + "," + (pieDim.h + 300) / 2 + ")");
 
-    // create function to draw the arcs of the pie slices.
-    var arc = d3.arc()
-        .outerRadius(pieDim.r - 10)
-        .innerRadius(0);
+        var w = 0;
+        var outerRadius = w / 2;
 
-    var labelArc = d3.arc()
-        .outerRadius(pieDim.r - 10)
-        .innerRadius(0);
+        // create function to draw the arcs of the pie slices.
+        var arc = d3.arc()
+            .outerRadius(pieDim.r - 10)
+            .innerRadius(0);
 
-    // create a function to compute the pie slice angles.
-    var pie = d3.pie().sort(null).value(function (d) {
-        return d.total;
-    });
+        var labelArc = d3.arc()
+            .outerRadius(pieDim.r - 10)
+            .innerRadius(0);
 
-    // Draw the pie slices.
-    piesvg.selectAll("path")
-        .data(pie(datarino))
-        .enter()
-        .append("path")
-        .attr("d", arc)
-        .each(function (d) {
-            this._current = d;
-        })
-        .attr("fill", function (d, i) {
-            return color(i);
+        // create a function to compute the pie slice angles.
+        var pie = d3.pie().sort(null).value(function (d) {
+            return d.total;
         });
 
-    var arcs = piesvg.selectAll("g.arc")
-        .data(pie(datarino))
-        .enter()
-        .append("g")
-        .attr("class", "arc")
+        // Draw the pie slices.
+        piesvg.selectAll("path")
+            .data(pie(data))
+            .enter()
+            .append("path")
+            .attr("d", arc)
+            .each(function (d) {
+                this._current = d;
+            })
+            .attr("fill", function (d, i) {
+                return color(i);
+            });
 
-    arcs.append("text")
-        .attr("transform", function (d) {
-            var c = arc.centroid(d),
-                x = c[0],
-                y = c[1],
-                // pythagorean theorem for hypotenuse
-                h = Math.sqrt(x * x + y * y);
-            console.log("translate(" + (x / h * pieDim.r) + ',' +
-                (y / h * pieDim.r) + ")");
-            return "translate(" + (x / h * pieDim.r) + ',' +
-                (y / h * pieDim.r) + ")";
-        })
-        .attr("text-anchor", function (d) {
-            // are we past the center?
-            return (d.endAngle + d.startAngle) / 2 > Math.PI ?
-                "end" : "start";
-        })
-        .text(function (d) {
-            return d.data.exception_name;
-        });
-    return pC;
+        var arcs = piesvg.selectAll("g.arc")
+            .data(pie(data))
+            .enter()
+            .append("g")
+            .attr("class", "arc")
+
+        arcs.append("text")
+            .attr("transform", function (d) {
+                var c = arc.centroid(d),
+                    x = c[0],
+                    y = c[1],
+                    // pythagorean theorem for hypotenuse
+                    h = Math.sqrt(x * x + y * y);
+                console.log("translate(" + (x / h * pieDim.r) + ',' +
+                    (y / h * pieDim.r) + ")");
+                return "translate(" + (x / h * pieDim.r) + ',' +
+                    (y / h * pieDim.r) + ")";
+            })
+            .attr("text-anchor", function (d) {
+                // are we past the center?
+                return (d.endAngle + d.startAngle) / 2 > Math.PI ?
+                    "end" : "start";
+            })
+            .text(function (d) {
+                return d.data.exception_name;
+            });
+        return pC;
+    })
 }
+
 
 function lineChart() {
     var margin = {top: 50, right: 50, bottom: 50, left: 50}
@@ -88,7 +89,7 @@ function lineChart() {
 
     var parseTime = d3.timeParse("%Y-%m-%dT%H:%M:%SZ");
 
-    d3.json('http://localhost:8000/api/v1/client/getExceptionsWithDateTime', function (d) {
+    d3.json(getDomain() +'/api/v1/client/getExceptionsWithDateTime', function (d) {
         d.forEach(function (d) {
             d.date = parseTime(d.date);
             d.count = +d.count;
---
+++ klog/staticlocal/js/properties.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Dual on 09/06/2017.
+ */
