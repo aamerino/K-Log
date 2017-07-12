d6d2796a4be897db8873646e1a39baaedba4245b
tsutsui <amerinotsutsui@gmail.com>
2017-07-12 15:41:08 +0200

    Adding properties

--- klog/bitbucketconector/repository.py
+++ klog/bitbucketconector/repository.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 from django.db.models import Count
 from django.db.models.functions import TruncDay
@@ -12,7 +14,7 @@ def getFork(src):
     full_route = 'main/java/com/amerino'
     full_route = full_route.split('/')
     r = requests.get("https://api.bitbucket.org/2.0/repositories/deixona/loggingexample/src/",
-                     auth=HTTPBasicAuth('deixona', 'ctT3B6j5CHfDhPKJfL5p'))
+                     auth=HTTPBasicAuth(os.environ["BITBUCKET_USER"], os.environ["BITBUCKET_CODE"]))
     araay = r.json()
     next_url = next(x for x in araay.get('values') if x.get('path') == 'src').get('links').get('self').get('href')
     next_path = 'src'
@@ -23,7 +25,7 @@ def getFork(src):
 def getRepoUrl(full_route, next_path, next_url):
     for currentFolder in full_route:
         next_path = next_path + '/' + currentFolder
-        r = requests.get(next_url, auth=HTTPBasicAuth('deixona', 'ctT3B6j5CHfDhPKJfL5p'))
+        r = requests.get(next_url, auth=HTTPBasicAuth(os.environ["BITBUCKET_USER"], os.environ["BITBUCKET_CODE"]))
         araay = r.json()
         next_url = next(x for x in araay.get('values') if x.get('path') == next_path).get('links').get('self').get(
             'href')
